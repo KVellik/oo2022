4e8f54e19c54a7945f2e8828369759ab88704695
KVellik <wellik@tlu.ee>
2022-02-14 14:55:25 +0200

    synnipaev

---
+++ 02_klass/kilpkonn.java
@@ -0,0 +1,37 @@
+public class kilpkonn{
+    private int x, y, suund;
+    //suund: 0 -> x++; 1 -> y++; 2 -> x--; 3 -> y--;
+    public String toString(){
+        return "("+x+", "+y+", suund "+suund+")";
+    }
+
+    public void edasi(){
+        if (suund == 0){
+            x++;
+        }
+        if (suund == 1){
+            y++;
+        }
+        if (suund == 2){
+            x--;
+        }
+        if (suund == 3){
+            y--;
+        }
+    }
+
+    public void keera(){
+        if (suund == 0){
+            suund++;
+        }
+        else if (suund == 1){
+            suund++;
+        }
+        else if (suund == 2){
+            suund++;
+        }
+        else if (suund == 3){
+            suund = 0;
+        }
+    }
+}
---
+++ 02_klass/proov2.java
@@ -0,0 +1,10 @@
+public class proov2{
+    public static void main(String[] arg){
+        kilpkonn k1 = new kilpkonn();
+        k1.edasi();
+        k1.edasi();
+        k1.keera();
+        k1.keera();
+        System.out.println(k1);
+    }
+}
